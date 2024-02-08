0fac915869ca0883d49cb5b3f15d8915e0af4ee6
Adam Leskis <leskis@gmail.com>
2024-02-08 16:26:30 +0000

    upgrade to standard ip, which is also in the free tier

--- main.tf
+++ main.tf
@@ -25,6 +25,7 @@ resource "azurerm_public_ip" "pip" {
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
   allocation_method   = "Static"
+  sku                 = "Standard"
 }
 
 resource "azurerm_network_interface" "main" {
