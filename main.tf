8e72ac72a77de548ab52433760face3380817c8a
Adam Leskis <leskis@gmail.com>
2024-04-17 15:54:35 +0100

    add in ipv6 capabilities

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "azurerm_resource_group" "main" {
 
 resource "azurerm_virtual_network" "main" {
   name                = "veilid-network"
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["10.0.0.0/16", "2600:db8:deca::/48"]
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
 }
@@ -15,13 +15,24 @@ resource "azurerm_subnet" "internal" {
   name                 = "veilid-subnet"
   resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.1.0/24"]
+  address_prefixes     = ["10.0.1.0/24", "2600:db8:deca::/64"]
 }
 
 
-resource "azurerm_public_ip" "pip" {
+resource "azurerm_public_ip" "pip-ipv4" {
   count               = local.instance_count
-  name                = "veilid-pip-${count.index}"
+  name                = "veilid-pip-ipv4-${count.index}"
+  ip_version          = "IPv4"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_public_ip" "pip-ipv6" {
+  count               = local.instance_count
+  name                = "veilid-pip-ipv6-${count.index}"
+  ip_version          = "IPv6"
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
   allocation_method   = "Static"
@@ -35,10 +46,19 @@ resource "azurerm_network_interface" "main" {
   location            = azurerm_resource_group.main.location
 
   ip_configuration {
-    name                          = "primary"
+    primary                       = true
+    name                          = "primary-ipv4"
     subnet_id                     = azurerm_subnet.internal.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.pip[count.index].id
+    public_ip_address_id          = azurerm_public_ip.pip-ipv4[count.index].id
+  }
+
+  ip_configuration {
+    name                          = "primary-ipv6"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+    private_ip_address_version    = "IPv6"
+    public_ip_address_id          = azurerm_public_ip.pip-ipv6[count.index].id
   }
 }
 
--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
-output "public_ip_address" {
-  value = [for ip in azurerm_public_ip.pip : ip.ip_address]
+output "public_ip_address_ipv4" {
+  value = [for ip in azurerm_public_ip.pip-ipv4 : ip.ip_address]
+}
+
+output "public_ip_address_ipv6" {
+  value = [for ip in azurerm_public_ip.pip-ipv6 : ip.ip_address]
 }
